e2365690ad862a5218c202506aba35dd119cc983
Lucas Eschechola <Lucas.eschechola@rockcontent.com>
2021-07-31 19:08:47 -0300

    Add FornecedorDTO

---
+++ Crud2/DTO/FornecedorDTO.cs
@@ -0,0 +1,48 @@
+﻿using Crud2.Enums;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Crud2.DTO
+{
+    public class FornecedorDTO
+    {
+        [Required(ErrorMessage = "O campo {0} é obrigatorio")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa conter entre {1} caracters á {2} ", MinimumLength = 3)]
+        public string Nome { get; set; }
+
+
+        [Required(ErrorMessage = "O campo {0} é obrigatorio")]
+        [StringLength(14, ErrorMessage = "O campo {0} precisa conter entre {1} caracters á {2} ", MinimumLength = 11)]
+        public string Documento { get; set; }
+
+        public TipoFornecedor TipoFornecedor { get; set; }
+
+        [DisplayName("Ativo ?")]
+        public bool Ativo { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatorio")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa conter entre {1} caracters á {2} ", MinimumLength = 3)]
+        public string Logradouro { get; set; }
+        [Required(ErrorMessage = "O campo {0} é obrigatorio")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa conter entre {1} caracters á {2} ", MinimumLength = 3)]
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatorio")]
+        [StringLength(8, ErrorMessage = "O campo {0} precisa conter entre {1} caracters á {2} ", MinimumLength = 8)]
+        public string CEP { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatorio")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa conter caracters {1} ", MinimumLength = 8)]
+        public string Bairro { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatorio")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa conter entre {1} caracters á {2} ", MinimumLength = 3)]
+        public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatorio")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa conter entre {1} caracters á {2} ", MinimumLength = 3)]
+        public string Estado { get; set; }
+
+    }
+}
